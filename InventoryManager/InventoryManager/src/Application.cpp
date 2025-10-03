57a01e0dacfa61523dcc9dd1d575c86d1273cb2a
Roberto Venezia <robertovenezia95@gmail.com>
2025-10-03 17:33:06 +0200

    Add item functionality

--- InventoryManager/InventoryManager/src/Application.cpp
+++ InventoryManager/InventoryManager/src/Application.cpp
@@ -33,25 +33,108 @@ bool InitializeDatabase(sqlite3*& db)
 		return false;
 	}
 
-	std::cout << "Database initialized successfully" << std::endl;
+	std::cout << "Database initialized successfully\n";
 	return true;
 }
 
+void AddItem(sqlite3* db)
+{
+	std::string name;
 
-int main() 
+	int quantity;
+	double price;
+
+	std::cout << "Enter item name: ";
+	std::cin.ignore();
+	std::getline(std::cin, name);
+
+	std::cout << "Enter quantity: ";
+	while (!(std::cin >> quantity || quantity < 0))
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Invalid input. Enter a positive integer for quantity: ";
+	}
+
+	std::cout << "Enter price: ";
+	while (!(std::cin >> price) || price < 0)
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Invalid input. Enter a positive number for price: ";
+	}
+
+
+	//SQL statement
+	const char* insertSQL = "INSERT INTO inventory (name, quantity, price) VALUES (?, ?, ?)";
+	sqlite3_stmt* statement;
+
+	int status = sqlite3_prepare_v2(db, insertSQL, -1, &statement, nullptr);
+	if (status != SQLITE_OK)
+	{
+		std::cerr << "Failed to prepare statement: " << sqlite3_errmsg(db) << std::endl;
+		return;
+	}
+
+	sqlite3_bind_text(statement, 1, name.c_str(), -1, SQLITE_STATIC);
+	sqlite3_bind_int(statement, 2, quantity);
+	sqlite3_bind_double(statement, 3, price);
+
+	//Execution
+	status = sqlite3_step(statement);
+	if (status != SQLITE_DONE)
+	{
+		std::cerr << "Failed to insert item: " << sqlite3_errmsg(db) << std::endl;
+	}
+	else
+	{
+		std::cout << "Item added successfully" << std::endl;
+	}
+
+	sqlite3_finalize(statement);
+}
+
+void DisplayMenu()
+{
+	std::cout << "\nInventory Management System\n";
+	std::cout << "1. Add Item\n";
+	std::cout << "2. List All Items\n";
+	std::cout << "3. Exit\n";
+	std::cout << "Enter your choice: ";
+}
+
+int main()
 {
 	sqlite3* db;
-	int rc = sqlite3_open("inventory.db", &db);
-	if (rc != SQLITE_OK) 
+
+	if (!InitializeDatabase(db))
 	{
-		std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
 		return 1;
 	}
-	std::cout << "Database opened successfully!\n";
 
-	InitializeDatabase(db);
+	int choice;
 
-	std::cin.get();
+	do
+	{
+		DisplayMenu();
+		std::cin >> choice;
+		switch (choice)
+		{
+			case 1: 
+				AddItem(db);
+				break;
+			case 2: 
+				std::cout << "Not available yet.\n";
+				break;
+			case 3: 
+				std::cout << "Closing program...";
+				break;
+			default:
+				std::cout << "Invalid input.\n";
+		}
+
+	} while (choice != 3);
+
 	sqlite3_close(db);
 	return 0;
 }
